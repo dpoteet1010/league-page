b1c76be4dcbc6bc3c51abe05d5ac4821b0497ca3
dpoteet1010 <poteetdavid29@gmail.com>
2025-03-27 23:02:57 -0500

    Update leagueInfo.js
    Changed league ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
+export const leagueID = "1129240411047956480"; // your league ID
 export const leagueName = "National Liver Failure League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
