3c3b02f38a3b4b659e8f08425ac913564cd24b04
dpoteet1010 <poteetdavid29@gmail.com>
2025-05-22 21:24:30 -0500

    Update index.svelte
    Changes to help debug

--- src/lib/Drafts/index.svelte
+++ src/lib/Drafts/index.svelte
@@ -1,9 +1,15 @@
 <script>
 	import { waitForAll } from '$lib/utils/helper';
-    import LinearProgress from '@smui/linear-progress';
-    import Draft from './Draft.svelte'; 
+	import LinearProgress from '@smui/linear-progress';
+	import Draft from './Draft.svelte';
 
-    export let upcomingDraftData, previousDraftsData, leagueTeamManagersData, playersData;
+	export let upcomingDraftData, previousDraftsData, leagueTeamManagersData, playersData;
+
+	// Debug logs to check data shape
+	console.log('upcomingDraftData:', upcomingDraftData);
+	console.log('previousDraftsData:', previousDraftsData);
+	console.log('leagueTeamManagersData:', leagueTeamManagersData);
+	console.log('playersData:', playersData);
 </script>
 
 <style>
@@ -12,53 +18,61 @@
 		width: 85%;
 		max-width: 500px;
 		margin: 80px auto;
+		text-align: center;
 	}
 
-    h4 {
-        text-align: center;
-    }
-
-    h6 {
-        text-align: center;
-    }
+	h4, h6 {
+		text-align: center;
+	}
 </style>
 
-<!-- Awaiting upcoming draft data -->
-{#await waitForAll(upcomingDraftData, leagueTeamManagersData, playersData) }
+<!-- Await upcoming draft data -->
+{#await waitForAll(upcomingDraftData, leagueTeamManagersData, playersData)}
 	<div class="loading">
 		<p>Retrieving upcoming draft...</p>
 		<br />
 		<LinearProgress indeterminate />
 	</div>
-{:then [upcomingDraft, leagueTeamManagers, {players}] }
-    <h4>Upcoming {upcomingDraft.year} Draft</h4>
-    <Draft draftData={upcomingDraft} {leagueTeamManagers} year={upcomingDraft.year} {players} />
+{:then [upcomingDraft, leagueTeamManagers, playersRaw]}
+	{#if upcomingDraft && leagueTeamManagers && playersRaw}
+		<h4>Upcoming {upcomingDraft.year} Draft</h4>
+		<Draft
+			draftData={upcomingDraft}
+			{leagueTeamManagers}
+			year={upcomingDraft.year}
+			players={playersRaw.players ?? playersRaw}
+		/>
+	{:else}
+		<p>Upcoming draft data is incomplete.</p>
+	{/if}
 {:catch error}
-	<!-- Error handling for upcoming drafts -->
-	<p>Something went wrong: {error.message}</p>
+	<p>Something went wrong loading upcoming draft: {error.message}</p>
 {/await}
 
-<!-- Awaiting previous drafts data -->
-{#await waitForAll(previousDraftsData, leagueTeamManagersData, playersData) }
+<!-- Await previous drafts data -->
+{#await waitForAll(previousDraftsData, leagueTeamManagersData, playersData)}
 	<div class="loading">
 		<p>Retrieving previous drafts...</p>
 		<br />
 		<LinearProgress indeterminate />
 	</div>
-{:then [previousDrafts, leagueTeamManagers, {players}] }
-	<!-- Only show previous drafts if there is data -->
+{:then [previousDrafts, leagueTeamManagers, playersRaw]}
 	{#if previousDrafts && previousDrafts.length > 0}
 		<hr />
 		<h4>Previous Drafts</h4>
 		{#each previousDrafts as previousDraft}
 			<h6>{previousDraft.year} Draft</h6>
-			<Draft draftData={previousDraft} previous={true} {leagueTeamManagers} year={previousDraft.year} {players} />
+			<Draft
+				draftData={previousDraft}
+				previous={true}
+				{leagueTeamManagers}
+				year={previousDraft.year}
+				players={playersRaw.players ?? playersRaw}
+			/>
 		{/each}
 	{:else}
-		<!-- If no previous drafts, show a message -->
 		<p>No previous drafts available.</p>
 	{/if}
 {:catch error}
-	<!-- Error handling for previous drafts -->
-	<p>Something went wrong: {error.message}</p>
+	<p>Something went wrong loading previous drafts: {error.message}</p>
 {/await}
