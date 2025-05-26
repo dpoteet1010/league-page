ebf00bb124c2e70e1f0a08181bdda92ab091c65b
dpoteet1010 <poteetdavid29@gmail.com>
2025-05-26 14:48:01 -0500

    Add files via upload

---
+++ src/lib/utils/helperFunctions/legacyLeagueUsers.js
@@ -0,0 +1,342 @@
+const legacyLeagueUsers = {
+  "2024": [
+    {
+      "avatar": "15d7cf259bc30eab8f6120f45f652fb6",
+      "display_name": "EvanCorzine",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Team Corzine"
+      },
+      "settings": null,
+      "user_id": "732863490657738752"
+    },
+    {
+      "avatar": "b3338675f635c2c1f42b469621d38ec6",
+      "display_name": "NicholasNewman",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "280 - A lady"
+      },
+      "settings": null,
+      "user_id": "862540586538921984"
+    },
+    {
+      "avatar": "b3338675f635c2c1f42b469621d38ec6",
+      "display_name": "AlecMiller",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Georgia Driving Academy"
+      },
+      "settings": null,
+      "user_id": "997207542067429376"
+    },
+    {
+      "avatar": "baa66adcc2f4dac7a7f97444d2208271",
+      "display_name": "JaredWestrich",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "The Naberhood"
+      },
+      "settings": null,
+      "user_id": "1002297734663237632"
+    },
+    {
+      "avatar": "8eb8f8bf999945d523f2c4033f70473e",
+      "display_name": "HarrisonHunt",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Been to Hill and Back"
+      },
+      "settings": null,
+      "user_id": "1128923498258219008"
+    },
+    {
+      "avatar": "b3338675f635c2c1f42b469621d38ec6",
+      "display_name": "TylerHammond",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Crashee Rice"
+      },
+      "settings": null,
+      "user_id": "1129215612842016768"
+    },
+    {
+      "avatar": "82aec8e811b839b8ec25d7b458afd57b",
+      "display_name": "RyanHaskin",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Olave's Garden"
+      },
+      "settings": null,
+      "user_id": "1129950475123781632"
+    },
+    {
+      "avatar": "875c88e2c8e80aac879cf51fcbc1d578",
+      "display_name": "JamesBoyd",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Ferguson Police"
+      },
+      "settings": null,
+      "user_id": "1137981174678462464"
+    },
+    {
+      "avatar": "578c6b253dd7b4bab45382e1af102204",
+      "display_name": "DavidPoteet",
+      "is_bot": false,
+      "is_owner": true,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Jordan Loves Children"
+      },
+      "settings": null,
+      "user_id": "1211171582181912576"
+    },
+    {
+      "avatar": "724ea9687ad966e30c2f0b6590692e15",
+      "display_name": "DanielKim",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Haskins Mom"
+      },
+      "settings": null,
+      "user_id": "1"
+    },
+    {
+      "avatar": "d22e52781a5d7a94bcbf2aa70e748382",
+      "display_name": "LukasBell",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Inmate P01135809"
+      },
+      "settings": null,
+      "user_id": "1227846679458230272"
+    },
+    {
+      "avatar": "d55d1f7075eda01948318de4af616075",
+      "display_name": "JonBerra",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2024",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "FJB"
+      },
+      "settings": null,
+      "user_id": "1228199076927909888"
+    }
+  ],
+  "2023": [
+    {
+      "avatar": "15d7cf259bc30eab8f6120f45f652fb6",
+      "display_name": "EvanCorzine",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Team Corzine"
+      },
+      "settings": null,
+      "user_id": "732863490657738752"
+    },
+    {
+      "avatar": "b3338675f635c2c1f42b469621d38ec6",
+      "display_name": "NicholasNewman",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Team Newsman"
+      },
+      "settings": null,
+      "user_id": "862540586538921984"
+    },
+    {
+      "avatar": "b3338675f635c2c1f42b469621d38ec6",
+      "display_name": "AlecMiller",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "From a QB TuaTard"
+      },
+      "settings": null,
+      "user_id": "997207542067429376"
+    },
+    {
+      "avatar": "baa66adcc2f4dac7a7f97444d2208271",
+      "display_name": "JaredWestrich",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Nacua's in Paris"
+      },
+      "settings": null,
+      "user_id": "1002297734663237632"
+    },
+    {
+      "avatar": "8eb8f8bf999945d523f2c4033f70473e",
+      "display_name": "HarrisonHunt",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "The Howelliban"
+      },
+      "settings": null,
+      "user_id": "1128923498258219008"
+    },
+    {
+      "avatar": "b3338675f635c2c1f42b469621d38ec6",
+      "display_name": "TylerHammond",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Koop Kupp Klan"
+      },
+      "settings": null,
+      "user_id": "1129215612842016768"
+    },
+    {
+      "avatar": "82aec8e811b839b8ec25d7b458afd57b",
+      "display_name": "RyanHaskin",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Hurts Dont It"
+      },
+      "settings": null,
+      "user_id": "1129950475123781632"
+    },
+    {
+      "avatar": "875c88e2c8e80aac879cf51fcbc1d578",
+      "display_name": "JamesBoyd",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Boyd"
+      },
+      "settings": null,
+      "user_id": "1137981174678462464"
+    },
+    {
+      "avatar": "578c6b253dd7b4bab45382e1af102204",
+      "display_name": "DavidPoteet",
+      "is_bot": false,
+      "is_owner": true,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Kraft's Playroom"
+      },
+      "settings": null,
+      "user_id": "1211171582181912576"
+    },
+    {
+      "avatar": "724ea9687ad966e30c2f0b6590692e15",
+      "display_name": "DanielKim",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "1-800-522-4700"
+      },
+      "settings": null,
+      "user_id": "1"
+    },
+    {
+      "avatar": "d22e52781a5d7a94bcbf2aa70e748382",
+      "display_name": "LukasBell",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Inmate P01135809"
+      },
+      "settings": null,
+      "user_id": "1227846679458230272"
+    },
+    {
+      "avatar": "d55d1f7075eda01948318de4af616075",
+      "display_name": "JonBerra",
+      "is_bot": false,
+      "is_owner": null,
+      "league_id": "2023",
+      "metadata": {
+        "allow_pn": "on",
+        "mention_pn": "on",
+        "team_name": "Record(er) Failure"
+      },
+      "settings": null,
+      "user_id": "1228199076927909888"
+    }
+  ]
+};
