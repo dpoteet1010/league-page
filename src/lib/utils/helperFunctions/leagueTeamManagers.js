5d193aee697cd4b39e083f2b2301bb1e9934cf1a
dpoteet1010 <poteetdavid29@gmail.com>
2025-05-26 14:53:16 -0500

    Update leagueTeamManagers.js
    Updating script to add legacy rosters/users

--- src/lib/utils/helperFunctions/leagueTeamManagers.js
+++ src/lib/utils/helperFunctions/leagueTeamManagers.js
@@ -4,67 +4,98 @@ import { teamManagersStore } from '$lib/stores';
 import { waitForAll } from './multiPromise';
 import { getManagers, getTeamData } from './universalFunctions';
 import { getLeagueData } from './leagueData';
+import { legacyLeagueUsers } from '$lib/data/legacyLeagueUsers';
+import { legacyLeagueRosters } from '$lib/data/legacyLeagueRosters';
 
 export const getLeagueTeamManagers = async () => {
-    if(get(teamManagersStore) && get(teamManagersStore).currentSeason) {
-		return get(teamManagersStore);
-	}
+    if (get(teamManagersStore) && get(teamManagersStore).currentSeason) {
+        return get(teamManagersStore);
+    }
+
     let currentLeagueID = leagueID;
-	let teamManagersMap = {};
+    let teamManagersMap = {};
     let finalUsers = {};
     let currentSeason = null;
 
-    // loop through all seasons and create a [year][roster_id]: team, managers object
-	while(currentLeagueID && currentLeagueID != 0) {
-		const [usersRaw, leagueData, rostersRaw] = await waitForAll(
-            fetch(`https://api.sleeper.app/v1/league/${currentLeagueID}/users`, {compress: true}),
-			getLeagueData(currentLeagueID),
-            fetch(`https://api.sleeper.app/v1/league/${currentLeagueID}/rosters`, {compress: true}),
+    // 1. Fetch current + historical data via Sleeper API
+    while (currentLeagueID && currentLeagueID != 0) {
+        const [usersRaw, leagueData, rostersRaw] = await waitForAll(
+            fetch(`https://api.sleeper.app/v1/league/${currentLeagueID}/users`, { compress: true }),
+            getLeagueData(currentLeagueID),
+            fetch(`https://api.sleeper.app/v1/league/${currentLeagueID}/rosters`, { compress: true }),
         ).catch((err) => { console.error(err); });
 
         const [users, rosters] = await waitForAll(
-            usersRaw.json(), 
-            rostersRaw.json(), 
+            usersRaw.json(),
+            rostersRaw.json(),
         ).catch((err) => { console.error(err); });
 
         const year = parseInt(leagueData.season);
         currentLeagueID = leagueData.previous_league_id;
-        if(!currentSeason) {
+        if (!currentSeason) {
             currentSeason = year;
         }
+
         teamManagersMap[year] = {};
         const processedUsers = processUsers(users);
 
-        // in order to not overwrite most recent data, only add new entries to finalUsers
-        for(const processedUserKey in processedUsers) {
-            if(finalUsers[processedUserKey]) continue;
-            finalUsers[processedUserKey] = processedUsers[processedUserKey];
+        // Add users to finalUsers map
+        for (const userID in processedUsers) {
+            if (!finalUsers[userID]) {
+                finalUsers[userID] = processedUsers[userID];
+            }
         }
-        for(const roster of rosters) {
+
+        for (const roster of rosters) {
             teamManagersMap[year][roster.roster_id] = {
                 team: getTeamData(processedUsers, roster.owner_id),
                 managers: getManagers(roster, processedUsers),
             };
         }
     }
+
+    // 2. Append legacy data from files
+    for (const year in legacyLeagueUsers) {
+        const seasonYear = parseInt(year);
+        const seasonUsers = legacyLeagueUsers[seasonYear] || {};
+        const seasonRosters = legacyLeagueRosters[seasonYear] || [];
+
+        teamManagersMap[seasonYear] = {};
+
+        // Add static users to finalUsers
+        for (const userID in seasonUsers) {
+            if (!finalUsers[userID]) {
+                finalUsers[userID] = seasonUsers[userID];
+            }
+        }
+
+        for (const roster of seasonRosters) {
+            teamManagersMap[seasonYear][roster.roster_id] = {
+                team: getTeamData(finalUsers, roster.owner_id),
+                managers: roster.managers.map(mid => finalUsers[mid]),
+            };
+        }
+    }
+
     const response = {
         currentSeason,
         teamManagersMap,
         users: finalUsers,
-    }
+    };
+
     teamManagersStore.update(() => response);
     return response;
-}
+};
 
 const processUsers = (rawUsers) => {
-	let finalUsers = {};
-	for(const user of rawUsers) {
+    let finalUsers = {};
+    for (const user of rawUsers) {
         user.user_name = user.user_name ?? user.display_name;
-		finalUsers[user.user_id] = user;
+        finalUsers[user.user_id] = user;
         const manager = managers.find(m => m.managerID === user.user_id);
-        if(manager) {
+        if (manager) {
             finalUsers[user.user_id].display_name = manager.name;
         }
-	}
-	return finalUsers;
-}
+    }
+    return finalUsers;
+};
