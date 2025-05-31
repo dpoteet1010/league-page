3431cc1cc16d501d4db2d3b0b496ff1d1f66ecfd
dpoteet1010 <poteetdavid29@gmail.com>
2025-05-31 17:45:41 -0500

    Update +page.js

--- src/routes/matchups/+page.js
+++ src/routes/matchups/+page.js
@@ -1,12 +1,24 @@
-import { getBrackets, getLeagueMatchups, getLeagueTeamManagers, loadPlayers } from '$lib/utils/helper';
+import {
+	getBrackets,
+	getLeagueMatchups,
+	getLeagueTeamManagers,
+	loadPlayers
+} from '$lib/utils/helper';
 
 export async function load({ url, fetch }) {
-    const queryWeek = url?.searchParams?.get('week');
-    return {
-        queryWeek: isNaN(queryWeek) ? null : queryWeek,
-        matchupsData: getLeagueMatchups(),
-        bracketsData: getBrackets(),
-        leagueTeamManagersData: getLeagueTeamManagers(),
-        playersData: loadPlayers(fetch),
-    };
+	const queryWeek = url?.searchParams?.get('week');
+
+	// Await all data here
+	const matchupsData = await getLeagueMatchups();
+	const bracketsData = await getBrackets();
+	const leagueTeamManagersData = await getLeagueTeamManagers();
+	const playersData = await loadPlayers(fetch);
+
+	return {
+		queryWeek: isNaN(queryWeek) ? null : queryWeek,
+		matchupsData,
+		bracketsData,
+		leagueTeamManagersData,
+		playersData
+	};
 }
