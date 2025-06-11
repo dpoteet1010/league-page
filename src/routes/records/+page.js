d53031808c24aaec2c91cdb221cb3cfd8e28244d
dpoteet1010 <poteetdavid29@gmail.com>
2025-06-10 21:16:18 -0500

    Update +page.js

--- src/routes/records/+page.js
+++ src/routes/records/+page.js
@@ -1,11 +1,11 @@
 import { getLeagueRecords, getLeagueTeamManagers, getLeagueTransactions, waitForAll } from '$lib/utils/helper';
 
 export async function load() {
-    const recordsInfo = waitForAll(
+    const recordsInfo = await waitForAll(
         getLeagueRecords(false),
         getLeagueTransactions(false),
         getLeagueTeamManagers(),
-    )
+    );
 
     return {
         recordsInfo
