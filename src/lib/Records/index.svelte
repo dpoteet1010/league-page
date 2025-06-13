fa6855eeec4297f066fb2e2ed0605410f2a17472
dpoteet1010 <poteetdavid29@gmail.com>
2025-06-12 20:59:20 -0500

    Update index.svelte

--- src/lib/Records/index.svelte
+++ src/lib/Records/index.svelte
@@ -1,170 +1,139 @@
 <script>
-	import Button, { Group, Label } from '@smui/button';
-	import { getLeagueRecords, getLeagueTransactions } from '$lib/utils/helper';
-	import AllTimeRecords from './AllTimeRecords.svelte';
-	import PerSeasonRecords from './PerSeasonRecords.svelte';
+    import Button, { Group, Label } from '@smui/button';
+    import { getLeagueRecords, getLeagueTransactions } from '$lib/utils/helper';
+    import AllTimeRecords from './AllTimeRecords.svelte';
+    import PerSeasonRecords from './PerSeasonRecords.svelte';
 
-	// Props
-	let { leagueData, totals, stale, leagueTeamManagers } = $props();
+    let {leagueData, totals, stale, leagueTeamManagers} = $props();;
 
-	// Reactive State
-	let leagueManagerRecords = $state();
-	let leagueRosterRecords = $state();
-	let leagueWeekHighs = $state();
-	let leagueWeekLows = $state();
-	let allTimeClosestMatchups = $state();
-	let allTimeBiggestBlowouts = $state();
-	let mostSeasonLongPoints = $state();
-	let leastSeasonLongPoints = $state();
-	let seasonWeekRecords = $state();
-	let currentYear = $state();
-	let lastYear = $state();
+    const refreshTransactions = async () => {
+        const newTransactions = await getLeagueTransactions(false, true);
+        totals = newTransactions.totals;
+    }
 
-	let key = $state('regularSeasonData');
-	let display = $state('allTime');
+    let leagueManagerRecords = $state();
+    let leagueRosterRecords = $state();
+    let leagueWeekHighs = $state();
+    let leagueWeekLows = $state();
+    let allTimeClosestMatchups = $state();
+    let allTimeBiggestBlowouts = $state();
+    let mostSeasonLongPoints = $state();
+    let leastSeasonLongPoints = $state();
+    let seasonWeekRecords = $state();
+    let currentYear = $state();
+    let lastYear = $state();
 
-	// Refresh Records
-	const refreshRecords = async () => {
-		const newRecords = await getLeagueRecords(true);
-		leagueData = newRecords;
-	};
+    const refreshRecords = async () => {
+        const newRecords = await getLeagueRecords(true);
 
-	// Refresh Transactions
-	const refreshTransactions = async () => {
-		const newTransactions = await getLeagueTransactions(false, true);
-		totals = newTransactions.totals;
-	};
+        // update values with new data
+        leagueData = newRecords;
+    }
 
-	// Run refresh if stale
-	if (stale) refreshTransactions();
-	if (leagueData?.stale) refreshRecords();
+    let key = $state("regularSeasonData");
 
-	// Reactive population of record data from selected league
-	$effect(() => {
-		if (!leagueData || !leagueData[key]) return;
+    $effect(() => {
+        if(!leagueData || !leagueData[key]) return;
 
-		const selectedLeagueData = leagueData[key];
+        const selectedLeagueData = leagueData[key];
 
-		leagueManagerRecords = selectedLeagueData.leagueManagerRecords;
-		leagueRosterRecords = selectedLeagueData.leagueRosterRecords;
-		leagueWeekHighs = selectedLeagueData.leagueWeekHighs;
-		leagueWeekLows = selectedLeagueData.leagueWeekLows;
-		allTimeClosestMatchups = selectedLeagueData.allTimeClosestMatchups;
-		allTimeBiggestBlowouts = selectedLeagueData.allTimeBiggestBlowouts;
-		mostSeasonLongPoints = selectedLeagueData.mostSeasonLongPoints;
-		leastSeasonLongPoints = selectedLeagueData.leastSeasonLongPoints;
-		seasonWeekRecords = selectedLeagueData.seasonWeekRecords;
-		currentYear = selectedLeagueData.currentYear;
-		lastYear = selectedLeagueData.lastYear;
-	});
+        leagueManagerRecords = selectedLeagueData.leagueManagerRecords;
+        leagueRosterRecords = selectedLeagueData.leagueRosterRecords;
+        leagueWeekHighs = selectedLeagueData.leagueWeekHighs;
+        leagueWeekLows = selectedLeagueData.leagueWeekLows;
+        allTimeClosestMatchups = selectedLeagueData.allTimeClosestMatchups;
+        allTimeBiggestBlowouts = selectedLeagueData.allTimeBiggestBlowouts;
+        mostSeasonLongPoints = selectedLeagueData.mostSeasonLongPoints;
+        leastSeasonLongPoints = selectedLeagueData.leastSeasonLongPoints;
+        seasonWeekRecords = selectedLeagueData.seasonWeekRecords;
+        currentYear = selectedLeagueData.currentYear;
+        lastYear = selectedLeagueData.lastYear;
+    });
+
+    if(stale) {
+        refreshTransactions();
+    }
+
+    if(leagueData.stale) {
+        refreshRecords();
+    }
+
+    let display = $state("allTime");
+
 </script>
 
 <style>
-	.rankingsWrapper {
-		margin: 0 auto;
-		width: 100%;
-		max-width: 1200px;
-	}
+    .rankingsWrapper {
+        margin: 0 auto;
+        width: 100%;
+        max-width: 1200px;
+    }
 
-	.empty {
-		margin: 10em 0 4em;
-		text-align: center;
-	}
+    .empty {
+        margin: 10em 0 4em;
+        text-align: center;
+    }
 
-	.buttonHolder {
-		text-align: center;
-		margin: 2em 0 0;
-	}
+    /* Button Styling */
+    .buttonHolder {
+        text-align: center;
+        margin: 2em 0 0;
+    }
 
-	@media (max-width: 540px) {
-		:global(.buttonHolder .selectionButtons) {
-			font-size: 0.6em;
-		}
-	}
+    /* Start button resizing */
 
-	@media (max-width: 415px) {
-		:global(.buttonHolder .selectionButtons) {
-			font-size: 0.5em;
-			padding: 0 6px;
-		}
-	}
+    @media (max-width: 540px) {
+        :global(.buttonHolder .selectionButtons) {
+            font-size: 0.6em;
+        }
+    }
 
-	@media (max-width: 315px) {
-		:global(.buttonHolder .selectionButtons) {
-			font-size: 0.45em;
-			padding: 0 3px;
-		}
-	}
+    @media (max-width: 415px) {
+        :global(.buttonHolder .selectionButtons) {
+            font-size: 0.5em;
+            padding: 0 6px;
+        }
+    }
+
+    @media (max-width: 315px) {
+        :global(.buttonHolder .selectionButtons) {
+            font-size: 0.45em;
+            padding: 0 3px;
+        }
+    }
+
+    /* End button resizing */
 </style>
 
 <div class="rankingsWrapper">
-	<!-- Debug Output -->
-	<pre>
-DEBUG:
-key = {key}
-display = {display}
-leagueData keys = {Object.keys(leagueData || {})}
-leagueRosterRecords: {JSON.stringify(leagueRosterRecords, null, 2)}
-</pre>
 
-	<div class="buttonHolder">
-		<Group variant="outlined">
-			<Button
-				class="selectionButtons"
-				on:click={() => (key = 'regularSeasonData')}
-				variant={key == 'regularSeasonData' ? 'raised' : 'outlined'}>
-				<Label>Regular Season</Label>
-			</Button>
-			<Button
-				class="selectionButtons"
-				on:click={() => (key = 'playoffData')}
-				variant={key == 'playoffData' ? 'raised' : 'outlined'}>
-				<Label>Playoffs</Label>
-			</Button>
-		</Group>
-		<br />
-		<Group variant="outlined">
-			<Button
-				class="selectionButtons"
-				on:click={() => (display = 'allTime')}
-				variant={display == 'allTime' ? 'raised' : 'outlined'}>
-				<Label>All-Time Records</Label>
-			</Button>
-			<Button
-				class="selectionButtons"
-				on:click={() => (display = 'season')}
-				variant={display == 'season' ? 'raised' : 'outlined'}>
-				<Label>Season Records</Label>
-			</Button>
-		</Group>
-	</div>
+    <div class="buttonHolder">
+        <Group variant="outlined">
+            <Button class="selectionButtons" onclick={() => key = "regularSeasonData"} variant="{key == "regularSeasonData" ? "raised" : "outlined"}">
+                <Label>Regular Season</Label>
+            </Button>
+            <Button class="selectionButtons" onclick={() => key = "playoffData"} variant="{key == "playoffData" ? "raised" : "outlined"}">
+                <Label>Playoffs</Label>
+            </Button>
+        </Group>
+        <br />
+        <Group variant="outlined">
+            <Button class="selectionButtons" onclick={() => display = "allTime"} variant="{display == "allTime" ? "raised" : "outlined"}">
+                <Label>All-Time Records</Label>
+            </Button>
+            <Button class="selectionButtons" onclick={() => display = "season"} variant="{display == "season" ? "raised" : "outlined"}">
+                <Label>Season Records</Label>
+            </Button>
+        </Group>
+    </div>
 
-	{#if display == 'allTime'}
-		{#if leagueWeekHighs?.length}
-			<AllTimeRecords
-				transactionTotals={totals}
-				{allTimeClosestMatchups}
-				{allTimeBiggestBlowouts}
-				{leagueManagerRecords}
-				{leagueWeekHighs}
-				{leagueWeekLows}
-				{leagueTeamManagers}
-				{mostSeasonLongPoints}
-				{leastSeasonLongPoints}
-				{key}
-			/>
-		{:else}
-			<p class="empty">No records <i>yet</i>...</p>
-		{/if}
-	{:else}
-		<PerSeasonRecords
-			transactionTotals={totals}
-			{leagueRosterRecords}
-			{seasonWeekRecords}
-			{leagueTeamManagers}
-			{currentYear}
-			{lastYear}
-			{key}
-		/>
-	{/if}
+    {#if display == "allTime"}
+        {#if leagueWeekHighs?.length}
+            <AllTimeRecords transactionTotals={totals} {allTimeClosestMatchups} {allTimeBiggestBlowouts} {leagueManagerRecords} {leagueWeekHighs} {leagueWeekLows} {leagueTeamManagers} {mostSeasonLongPoints} {leastSeasonLongPoints} {key} />
+        {:else}
+            <p class="empty">No records <i>yet</i>...</p>
+        {/if}
+    {:else}
+        <PerSeasonRecords transactionTotals={totals} {leagueRosterRecords} {seasonWeekRecords} {leagueTeamManagers} {currentYear} {lastYear} {key} />
+    {/if}
 </div>
