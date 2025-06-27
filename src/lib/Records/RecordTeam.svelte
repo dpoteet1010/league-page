b94e854b4095d70dc65a9642fc689810f0560e35
dpoteet1010 <poteetdavid29@gmail.com>
2025-06-26 19:30:50 -0500

    Update RecordTeam.svelte

--- src/lib/Records/RecordTeam.svelte
+++ src/lib/Records/RecordTeam.svelte
@@ -1,34 +1,13 @@
 <script>
-	import { onMount } from 'svelte';
 	import { getAvatarFromTeamManagers, getTeamNameFromTeamManagers, renderManagerNames } from "$lib/utils/helperFunctions/universalFunctions";
 
-	export let leagueTeamManagers, managerID = null, rosterID = null, year, compressed = false, points = null;
+    export let leagueTeamManagers, managerID = null, rosterID = null, year, compressed = false, points = null;
 
-	let user = null;
-	let logMessages = [];
+    let user = null;
 
-	function log(message, data = null) {
-		const output = data ? `${message}: ${JSON.stringify(data)}` : message;
-		logMessages = [...logMessages, output]; // reassign for Svelte reactivity
-		console.log(output);
-	}
-
-	onMount(() => {
-		if (managerID && leagueTeamManagers?.users?.[managerID]) {
-			user = leagueTeamManagers.users[managerID];
-			log("Found user from managerID", user);
-		} else if (rosterID) {
-			const avatar = getAvatarFromTeamManagers(leagueTeamManagers, rosterID, year);
-			const teamName = getTeamNameFromTeamManagers(leagueTeamManagers, rosterID, year);
-			const managerNames = renderManagerNames(leagueTeamManagers, rosterID, year);
-
-			log("Resolved avatar from rosterID", avatar);
-			log("Resolved teamName from rosterID", teamName);
-			log("Resolved managerNames from rosterID", managerNames);
-		} else {
-			log("No managerID or rosterID provided");
-		}
-	});
+    if(managerID) {
+        user = leagueTeamManagers.users[managerID];
+    }
 </script>
 
 <style>
@@ -40,85 +19,67 @@
 		border: 0.25px solid #777;
 	}
 
-	.recordTeam {
-		display: flex;
-	}
+    .recordTeam {
+        display: flex;
+    }
 
-	.name {
-		margin: auto 0;
-	}
+    .name {
+        margin: auto 0;
+    }
 
-	.managerNames {
-		font-size: 0.75em;
-		font-style: italic;
-		color: var(--g999);
-		max-width: 180px;
-		white-space: normal;
-		text-align: left;
-	}
+    .managerNames {
+        font-size: 0.75em;
+        font-style: italic;
+        color: var(--g999);
+        max-width: 180px;
+        white-space: normal;
+        text-align: left;
+    }
 
-	.compressed {
+    .compressed {
 		height: 30px;
 		margin-right: 6px;
-	}
+    }
 
-	@media (max-width: 405px) {
-		.teamAvatar {
-			height: 25px;
-			margin-right: 8px;
-		}
-		.compressed {
-			height: 20px;
-			margin-right: 4px;
-		}
-	}
+    @media (max-width: 405px) {
+        .teamAvatar {
+            height: 25px;
+            margin-right: 8px;
+        }
 
-	@media (max-width: 295px) {
-		.teamAvatar {
-			display: none;
-		}
-	}
+        .compressed {
+            height: 20px;
+            margin-right: 4px;
+        }
+    }
 
-	.log-output {
-		background-color: #222;
-		color: #ccc;
-		font-size: 0.75em;
-		padding: 0.75em;
-		margin-top: 1em;
-		border-radius: 5px;
-		border: 1px solid #444;
-		white-space: pre-wrap;
-	}
+    @media (max-width: 295px) {
+        .teamAvatar {
+            display: none;
+        }
+    }
+
 </style>
 
 <div class="recordTeam">
-	{#if user}
-		<img alt="team avatar" class="teamAvatar{compressed ? ' compressed' : ''}" src={`https://sleepercdn.com/avatars/thumbs/${user.avatar}`} />
-	{:else if rosterID}
-		<img alt="team avatar" class="teamAvatar{compressed ? ' compressed' : ''}" src={getAvatarFromTeamManagers(leagueTeamManagers, rosterID, year)} />
-	{/if}
-
-	<span class="name">
-		<div class="teamName">
-			{#if user}
-				{user.display_name}
-			{:else if rosterID}
-				{getTeamNameFromTeamManagers(leagueTeamManagers, rosterID, year)}
-				{points ? ` (${points})` : ""}
-			{/if}
-		</div>
-
-		{#if !user && rosterID}
-			<div class="managerNames">
-				{renderManagerNames(leagueTeamManagers, rosterID, year)}
-			</div>
-		{/if}
-	</span>
+    {#if user}
+        <img alt="team avatar" class="teamAvatar{compressed ? " compressed" : ""}" src="{`https://sleepercdn.com/avatars/thumbs/${user.avatar}`}" />
+    {:else if rosterID}
+        <img alt="team avatar" class="teamAvatar{compressed ? " compressed" : ""}" src="{getAvatarFromTeamManagers(leagueTeamManagers, rosterID, year)}" />
+    {/if}
+    <span class="name">
+        <div class="teamName">
+            {#if user}
+                {user.display_name}
+            {:else if rosterID}
+                {getTeamNameFromTeamManagers(leagueTeamManagers, rosterID, year)}
+                {points ? ` (${points})` : ""}
+            {/if}
+        </div>
+        {#if !user}
+            <div class="managerNames">
+                {renderManagerNames(leagueTeamManagers, rosterID, year)}
+            </div>
+        {/if}
+    </span>
 </div>
-
-{#if logMessages.length}
-	<div class="log-output">
-		<h4>Debug Logs</h4>
-		<pre>{logMessages.join('\n')}</pre>
-	</div>
-{/if}
