d9f1141d00193b3607ca44477ebc886c255abc7f
dpoteet1010 <poteetdavid29@gmail.com>
2025-09-10 15:11:21 -0500

    Update CreateComment.svelte

--- src/lib/BlogPosts/CreateComment.svelte
+++ src/lib/BlogPosts/CreateComment.svelte
@@ -1,51 +1,41 @@
 <script>
     import Button, {Label} from "@smui/button";
     import Textfield from "@smui/textfield";
-	import { createEventDispatcher } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
 
-    export let showWrite;
-
-	const dispatch = createEventDispatcher();
+    export let showWrite = false;
+    const dispatch = createEventDispatcher();
 
     let comment = '';
     let author = '';
 
-    const submit = () => {
-        console.log("Child submit clicked. Comment:", comment, "Author:", author); // <-- log
-        dispatch('createComment', {comment, author});
-    }
-
     const toggleShow = () => {
         showWrite = !showWrite;
-        console.log("Toggled showWrite:", showWrite); // <-- log
+    }
+
+    const submit = () => {
+        console.log("Dispatching createComment event:", { comment, author });
+        dispatch('createComment', { comment, author });
     }
 </script>
 
 {#if showWrite}
-    <div style="text-align:center;">
-        <textarea 
-            bind:value={comment} 
-            placeholder="Leave a comment..." 
-            style="width:99%; height:7em; font-size:1rem;"
-        ></textarea>
+    <div class="textBoxHolder">
+        <textarea autofocus bind:value={comment} class="commentTextBox" placeholder="Leave a comment..."/>
     </div>
 
-    <div style="margin: 1em 0;">
-        <Textfield 
-            bind:value={author} 
-            label="Your Sleeper Username" 
-            style="width:100%;" 
-        />
-        <Button onclick={submit} variant="unelevated">
+    <div class="submitArea">
+        <Textfield bind:value={author} label="Your Sleeper Username" variant="outlined" />
+        <Button on:click={submit} variant="unelevated">
             <Label>Submit Comment</Label>
         </Button>
-        <Button onclick={toggleShow} color="secondary" variant="unelevated">
+        <Button on:click={toggleShow} color="secondary" variant="unelevated">
             <Label>Cancel</Label>
         </Button>
     </div>
 {:else}
-    <div style="margin: 1em 0;">
-        <Button onclick={toggleShow} variant="unelevated">
+    <div class="submitArea">
+        <Button on:click={toggleShow} variant="unelevated">
             <Label>Leave a Comment</Label>
         </Button>
     </div>
